68131f30f9826cce01a27f64562ad182d9474d34
edunga1 <goonr21@gmail.com>
2022-11-14 15:01:08 +0900

    Add mypy as python lsp diagnostic

--- vim/lua/lsp/python.lua
+++ vim/lua/lsp/python.lua
@@ -2,6 +2,7 @@ local null_ls = require 'null-ls'
 return {
     server = require'lspconfig'.pyright,
     sources = {
+        null_ls.builtins.diagnostics.mypy,
         null_ls.builtins.formatting.autopep8,
         null_ls.builtins.formatting.isort,
     },
