644e0ea954572f1b34c87d816b91d7d467e11954
edunga1 <goonr21@gmail.com>
2022-11-22 11:08:38 +0900

    Add a argument `--ignore-missing-imports` to mypy

--- vim/lua/lsp/python.lua
+++ vim/lua/lsp/python.lua
@@ -1,11 +1,12 @@
 local null_ls = require 'null-ls'
 return {
-    server = require'lspconfig'.pyright,
+    server = require 'lspconfig'.pyright,
     sources = {
         null_ls.builtins.diagnostics.pylint,
-        null_ls.builtins.diagnostics.mypy,
+        null_ls.builtins.diagnostics.mypy.with {
+            extra_args = { '--ignore-missing-imports' }
+        },
         null_ls.builtins.formatting.autopep8,
         null_ls.builtins.formatting.isort,
     },
 }
-
