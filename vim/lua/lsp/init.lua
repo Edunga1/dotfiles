75ddcdd6bd3820e958807ff684de6a771f51f558
edunga1 <goonr21@gmail.com>
2023-01-09 11:44:23 +0900

    Update lsp init.lua to show source in diagnostic floating window

--- vim/lua/lsp/init.lua
+++ vim/lua/lsp/init.lua
@@ -5,6 +5,11 @@ require 'mason'.setup{
 require 'mason-lspconfig'.setup()
 
 -- setup key mappings
+vim.diagnostic.config({
+    float = {
+      source = true,
+    },
+})
 local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
