caa671ed25ef8b43f2ad42c106ce8439cffe08e9
edunga1 <goonr21@gmail.com>
2023-03-06 18:34:38 +0900

    Update hammerspoon
    Migrate my window arrangement to miro windows manager

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,5 +1,16 @@
 hs.hotkey.bind({'cmd', 'shift'}, 'r', function() hs.reload() end)
 
 require('modules.inputsource-aurora')
-require('modules.window-arrangement')
 require('modules.window-resize')
+
+-- Initialize the window manager
+local hyper = {"ctrl", "shift"}
+hs.loadSpoon("MiroWindowsManager")
+spoon.MiroWindowsManager:bindHotkeys({
+  up = {hyper, "up"},
+  right = {hyper, "right"},
+  down = {hyper, "down"},
+  left = {hyper, "left"},
+  fullscreen = {hyper, "f"},
+  nextscreen = {hyper, "n"}
+})
