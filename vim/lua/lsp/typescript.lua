8d8e202c2836a1f2067dc0ae47722a7df2a54f55
edunga1 <goonr21@gmail.com>
2023-04-03 15:04:54 +0000

    Add `:RenameFile` command to rename file for typescript

--- vim/lua/lsp/typescript.lua
+++ vim/lua/lsp/typescript.lua
@@ -1,8 +1,51 @@
 local common = require 'lsp._common'
 
+local function rename_file()
+  local source_file, target_file
+
+  vim.ui.input({
+    prompt = "Source : ",
+    completion = "file",
+    default = vim.api.nvim_buf_get_name(0)
+  },
+    function(input)
+      source_file = input
+    end
+  )
+  vim.ui.input({
+    prompt = "Target : ",
+    completion = "file",
+    default = source_file
+  },
+    function(input)
+      target_file = input
+    end
+  )
+
+  local params = {
+    command = "_typescript.applyRenameFile",
+    arguments = {
+      {
+        sourceUri = source_file,
+        targetUri = target_file,
+      },
+    },
+    title = ""
+  }
+
+  vim.lsp.util.rename(source_file, target_file)
+  vim.lsp.buf.execute_command(params)
+end
+
 require 'lspconfig'.tsserver.setup {
   on_attach = common.on_attach,
   capabilities = common.capabilities,
+  commands = {
+    RenameFile = {
+      rename_file,
+      description = "Rename file",
+    },
+  },
 }
 
 local null_ls = require 'null-ls'
