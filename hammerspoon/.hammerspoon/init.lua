d15d352d7891d8102c3d5faafdc788c345e6c4d0
edunga1 <goonr21@gmail.com>
2023-11-13 15:10:18 +0900

    Update hammerspoon windows resizer hotkeys
    moved hyper to ctrl + shift + option from ctrl + option.
    it was confused becuase `MiroWindowsManager` uses ctrl + shift as hyper

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -4,13 +4,13 @@ require('modules.inputsource-aurora')
 require('modules.window-resize')
 
 -- Initialize the window manager
-local hyper = {"ctrl", "shift"}
-hs.loadSpoon("MiroWindowsManager")
+local hyper = {'ctrl', 'shift'}
+hs.loadSpoon('MiroWindowsManager')
 spoon.MiroWindowsManager:bindHotkeys({
-  up = {hyper, "up"},
-  right = {hyper, "right"},
-  down = {hyper, "down"},
-  left = {hyper, "left"},
-  fullscreen = {hyper, "f"},
-  nextscreen = {hyper, "n"}
+  up = {hyper, 'up'},
+  right = {hyper, 'right'},
+  down = {hyper, 'down'},
+  left = {hyper, 'left'},
+  fullscreen = {hyper, 'f'},
+  nextscreen = {hyper, 'n'}
 })
--- hammerspoon/.hammerspoon/modules/window-resize.lua
+++ hammerspoon/.hammerspoon/modules/window-resize.lua
@@ -17,7 +17,7 @@ local right = 50
 local top = -100
 local down = 100
 
-local mod = {'control', 'option'}
+local mod = {'ctrl', 'shift', 'option'}
 hs.hotkey.bind(mod, 'left', resize_win(left, 0))
 hs.hotkey.bind(mod, 'right', resize_win(right, 0))
 hs.hotkey.bind(mod, 'up', resize_win(0, top))
