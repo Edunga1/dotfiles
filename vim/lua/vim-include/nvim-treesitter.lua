38625bbb8b9661f90778bda80e4440c6edc8fce0
edunga1 <goonr21@gmail.com>
2023-11-26 12:39:45 +0000

    Remove shortcuts to go to the next/previous condition block

--- vim/lua/vim-include/nvim-treesitter.lua
+++ vim/lua/vim-include/nvim-treesitter.lua
@@ -52,12 +52,6 @@ require'nvim-treesitter.configs'.setup {
         ["[M"] = "@function.outer",
         ["[]"] = "@class.outer",
       },
-      goto_next = {
-        ["]d"] = "@conditional.outer",
-      },
-      goto_previous = {
-        ["[d"] = "@conditional.outer",
-      }
     },
   },
 }
