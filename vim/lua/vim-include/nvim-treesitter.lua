72eaee9cdf465d6a60af9c2d009bd24bcebd75e3
edunga1 <goonr21@gmail.com>
2023-12-21 12:09:44 +0900

    Update treesitter config
    disable auto-install. It is annoying to get error messages when network stuff is bad
    removed auto-install ignore list
    enabled highlight

--- vim/lua/vim-include/nvim-treesitter.lua
+++ vim/lua/vim-include/nvim-treesitter.lua
@@ -1,11 +1,8 @@
 require'nvim-treesitter.configs'.setup {
   sync_install = false,
-  auto_install = true,
-  ignore_install = {
-    "gitcommit",  -- looks not good. diff is not highlighted
-  },
+  auto_install = false,
   highlight = {
-    enable = false,
+    enable = true,
   },
 
   -- nvim-treesitter-textobjects
