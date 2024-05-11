fb7522331171163a8c9a24e4c7ffc88c15ccd41e
edunga1 <goonr21@gmail.com>
2024-05-11 21:07:10 +0000

    Update lsp workspace folder list keymap to use fzf

--- vim/lua/lsp/servers/utils/common.lua
+++ vim/lua/lsp/servers/utils/common.lua
@@ -13,7 +13,19 @@ local on_attach = function(client, bufnr)
   vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
   vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
   vim.keymap.set('n', '<space>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    vim.fn['fzf#run']({
+      source = vim.lsp.buf.list_workspace_folders(),
+      options = {
+        '--prompt', 'Remove workspace folder > ',
+      },
+      window = {
+        width = 0.8,
+        height = 0.8,
+      },
+      sink = function(line)
+        vim.lsp.buf.remove_workspace_folder(line)
+      end
+    })
   end, bufopts)
   vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
   vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
