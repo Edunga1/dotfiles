d2a83258e07df2e7192aa8ecbe7279b497871844
edunga1 <goonr21@gmail.com>
2024-08-11 09:05:23 +0000

    Remove astro lsp server

--- vim/lua/lsp/servers/astro.lua
+++
@@ -1,8 +0,0 @@
-local common = require 'lsp.servers.utils.common'
-
-return function(_, lspconfig)
-  lspconfig.astro.setup {
-    on_attach = common.on_attach,
-    capabilities = common.capabilities,
-  }
-end
