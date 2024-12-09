b3a382aba6db3063976ffc0f09cde832deb65f82
edunga1 <goonr21@gmail.com>
2024-12-09 13:59:04 +0900

    Remove a comment

--- vim/lua/lsp/servers/gdscript.lua
+++ vim/lua/lsp/servers/gdscript.lua
@@ -1,6 +1,5 @@
 local common = require 'lsp.servers.utils.common'
 
--- doesn't work on WSL. check later
 return function(_, lspconfig)
   lspconfig.gdscript.setup {
     on_attach = common.on_attach,
