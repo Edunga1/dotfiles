5e0ce3cae86b1d4e226358a4c605f6bc89359b84
edunga1 <goonr21@gmail.com>
2025-04-14 15:05:19 +0900

    Adjust inputsource-aurora size
    increased the height of the aurora box to 25 pixels
    refactor code

--- hammerspoon/.hammerspoon/modules/inputsource-aurora.lua
+++ hammerspoon/.hammerspoon/modules/inputsource-aurora.lua
@@ -2,22 +2,13 @@
 
 local boxes = {}
 local inputEnglish = "com.apple.keylayout.ABC"
-local box_height = 10
+local box_height = 25
 local box_alpha = 0.35
 local GREEN = hs.drawing.color.osx_green
-
-local function newBox()
-    return hs.drawing.rectangle(hs.geometry.rect(0,0,0,0))
-end
-
-local function reset_boxes()
-    boxes = {}
-end
 
 local function draw_rectangle(target_draw, x, y, width, height, fill_color)
   target_draw:setSize(hs.geometry.rect(x, y, width, height))
   target_draw:setTopLeft(hs.geometry.point(x, y))
-
   target_draw:setFillColor(fill_color)
   target_draw:setFill(true)
   target_draw:setAlpha(box_alpha)
@@ -27,30 +18,37 @@ local function draw_rectangle(target_draw, x, y, width, height, fill_color)
   target_draw:show()
 end
 
-local function enable_show()
-    reset_boxes()
-    hs.fnutils.each(hs.screen.allScreens(), function(scr)
-        local frame = scr:fullFrame()
-
-        local box = newBox()
-        draw_rectangle(box, frame.x, frame.y, frame.w, box_height, GREEN)
-        table.insert(boxes, box)
-    end)
+local function create_box(screen)
+  local frame = screen:fullFrame()
+  local box = hs.drawing.rectangle(hs.geometry.rect(0,0,0,0))
+  draw_rectangle(box, frame.x, frame.y, frame.w, box_height, GREEN)
+  table.insert(boxes, box)
 end
 
-local function disable_show()
-    hs.fnutils.each(boxes, function(box)
-        if box ~= nil then
-            box:delete()
-        end
-    end)
-    reset_boxes()
+local function reset_boxes()
+  hs.fnutils.each(boxes, function(box)
+    if box ~= nil then
+      box:delete()
+    end
+  end)
+  boxes = {}
+end
+
+local function show_aurora()
+  reset_boxes()
+  hs.fnutils.each(hs.screen.allScreens(), function(screen)
+    create_box(screen)
+  end)
+end
+
+local function hide_aurora()
+  reset_boxes()
 end
 
 -- 입력소스 변경 이벤트에 이벤트 리스너를 달아준다
 hs.keycodes.inputSourceChanged(function()
-    disable_show()
-    if hs.keycodes.currentSourceID() ~= inputEnglish then
-        enable_show()
-    end
+  hide_aurora()
+  if hs.keycodes.currentSourceID() ~= inputEnglish then
+    show_aurora()
+  end
 end)
