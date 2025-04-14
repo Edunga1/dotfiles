e234274e9b7cbfbd2d0f395baa930e05ae646b21
edunga1 <goonr21@gmail.com>
2025-04-14 09:34:13 +0900

    Refactor hammerspoon inputsource-aurora.lua

--- hammerspoon/.hammerspoon/modules/inputsource-aurora.lua
+++ hammerspoon/.hammerspoon/modules/inputsource-aurora.lua
@@ -6,43 +6,15 @@ local box_height = 10
 local box_alpha = 0.35
 local GREEN = hs.drawing.color.osx_green
 
--- 입력소스 변경 이벤트에 이벤트 리스너를 달아준다
-hs.keycodes.inputSourceChanged(function()
-    disable_show()
-    if hs.keycodes.currentSourceID() ~= inputEnglish then
-        enable_show()
-    end
-end)
-
-function enable_show()
-    reset_boxes()
-    hs.fnutils.each(hs.screen.allScreens(), function(scr)
-        local frame = scr:fullFrame()
-
-        local box = newBox()
-        draw_rectangle(box, frame.x, frame.y, frame.w, box_height, GREEN)
-        table.insert(boxes, box)
-    end)
-end
-
-function disable_show()
-    hs.fnutils.each(boxes, function(box)
-        if box ~= nil then
-            box:delete()
-        end
-    end)
-    reset_boxes()
-end
-
-function newBox()
+local function newBox()
     return hs.drawing.rectangle(hs.geometry.rect(0,0,0,0))
 end
 
-function reset_boxes()
+local function reset_boxes()
     boxes = {}
 end
 
-function draw_rectangle(target_draw, x, y, width, height, fill_color)
+local function draw_rectangle(target_draw, x, y, width, height, fill_color)
   target_draw:setSize(hs.geometry.rect(x, y, width, height))
   target_draw:setTopLeft(hs.geometry.point(x, y))
 
@@ -54,3 +26,31 @@ function draw_rectangle(target_draw, x, y, width, height, fill_color)
   target_draw:setBehavior(hs.drawing.windowBehaviors.canJoinAllSpaces)
   target_draw:show()
 end
+
+local function enable_show()
+    reset_boxes()
+    hs.fnutils.each(hs.screen.allScreens(), function(scr)
+        local frame = scr:fullFrame()
+
+        local box = newBox()
+        draw_rectangle(box, frame.x, frame.y, frame.w, box_height, GREEN)
+        table.insert(boxes, box)
+    end)
+end
+
+local function disable_show()
+    hs.fnutils.each(boxes, function(box)
+        if box ~= nil then
+            box:delete()
+        end
+    end)
+    reset_boxes()
+end
+
+-- 입력소스 변경 이벤트에 이벤트 리스너를 달아준다
+hs.keycodes.inputSourceChanged(function()
+    disable_show()
+    if hs.keycodes.currentSourceID() ~= inputEnglish then
+        enable_show()
+    end
+end)
