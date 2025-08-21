ab7d7ff08d5cbe6a04bcfce6e2a85c1b1e510271
edunga1 <goonr21@gmail.com>
2025-08-21 17:29:06 +0900

    Remove vim workspace commands
    - unuseful commands

--- vim/lua/lsp/servers/utils/common.lua
+++ vim/lua/lsp/servers/utils/common.lua
@@ -15,23 +15,6 @@ local on_attach = function(_, bufnr)
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wl', function()
-    vim.fn['fzf#run']({
-      source = vim.lsp.buf.list_workspace_folders(),
-      options = {
-        '--prompt', 'Remove workspace folder > ',
-      },
-      window = {
-        width = 0.8,
-        height = 0.8,
-      },
-      sink = function(line)
-        vim.lsp.buf.remove_workspace_folder(line)
-      end
-    })
-  end, bufopts)
   vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
   vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
   vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
