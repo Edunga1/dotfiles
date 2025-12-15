6e5189ae4f72e5805f2d98198ea73215de1462d0
edunga1 <goonr21@gmail.com>
2025-12-16 00:21:21 +0900

    Revert CopilotChat floating window settings

--- vim/lua/vim-include/copilot-chat-nvim.lua
+++ vim/lua/vim-include/copilot-chat-nvim.lua
@@ -1,11 +1,4 @@
 require("CopilotChat").setup {
-  window = {
-    layout = 'float',
-    relative = 'cursor',
-    width = 1,
-    height = 0.4,
-    row = 1,
-  }
 }
 
 vim.keymap.set('n', '<M-a>', ':CopilotChatToggle<CR>', { noremap = true })
