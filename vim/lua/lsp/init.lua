a4728eed201d1db7ef440846582cd1e48b923bee
edunga1 <goonr21@gmail.com>
2026-06-17 17:26:00 +0900

    Remove vim lsp default keymaps
    - The custom keymap has been provided as the default nvim keymap
    - K -> K, gi -> gri, <space>D -> grt, <space>rn -> grn, <space>ca -> gra, gr -> grr

--- vim/lua/lsp/init.lua
+++ vim/lua/lsp/init.lua
@@ -55,32 +55,9 @@ vim.api.nvim_create_autocmd('LspAttach', {
     -- Mappings.
     -- See `:help vim.lsp.*` for documentation on any of the below functions
     local bufopts = { noremap = true, silent = true, buffer = bufnr }
-    local function on_list(options)
-      vim.fn.setqflist({}, ' ', options)
-      vim.api.nvim_command('cfirst')
-    end
-    local context = {
-      includeDeclaration = false,  -- do not include declaration in references
-    }
-
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-    vim.keymap.set('n', 'gr',
-      function()
-        vim.lsp.buf.references(
-          context,
-          { on_list = on_list }
-        )
-      end,
-      bufopts
-    )
-    vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
-    vim.keymap.set('v', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+    vim.keymap.set({'n', 'x'}, '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
   end,
 })
 
