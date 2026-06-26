3389e9b41aeee9b237a7ea664351db49a786fde5
edunga1 <goonr21@gmail.com>
2026-06-26 09:12:09 +0900

    Refactor vim ts_ls rename_file to ts_ls.lua

--- vim/after/lsp/ts_ls.lua
+++ vim/after/lsp/ts_ls.lua
@@ -1,7 +1,49 @@
+local function rename_file()
+  local source_file, target_file
+
+  vim.ui.input(
+    {
+      prompt = "Source : ",
+      completion = "file",
+      default = vim.api.nvim_buf_get_name(0)
+    },
+    function(input)
+      source_file = input
+    end
+  )
+  vim.ui.input(
+    {
+      prompt = "Target : ",
+      completion = "file",
+      default = source_file
+    },
+    function(input)
+      target_file = input
+    end
+  )
+
+  local params = {
+    command = "_typescript.applyRenameFile",
+    arguments = {
+      {
+        sourceUri = source_file,
+        targetUri = target_file,
+      },
+    },
+    title = ""
+  }
+
+  vim.lsp.util.rename(source_file, target_file)
+  local client = vim.lsp.get_clients({ bufnr = 0, name = 'ts_ls' })[1]
+  if client then
+    client:exec_cmd(params)
+  end
+end
+
 return {
   on_attach = function(_, buffer)
     -- Enable file renaming command
-    vim.api.nvim_buf_create_user_command(buffer, 'RenameFile', require('lsp.utils').rename_file, {
+    vim.api.nvim_buf_create_user_command(buffer, 'RenameFile', rename_file, {
       desc = "Rename file",
     })
   end,
