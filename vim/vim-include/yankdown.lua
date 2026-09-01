03d97ec972524e01dbd728dfc7ab914cdacbe6a0
edunga1 <goonr21@gmail.com>
2026-09-01 09:35:44 +0900

    Replace yankdown.nvim mapping
    "gp" to paste

--- vim/vim-include/yankdown.lua
+++ vim/vim-include/yankdown.lua
@@ -2,6 +2,12 @@ if vim.g.plugs["yankdown.nvim"] == nil then
   return
 end
 
-require("yankdown").setup({
-  auto_intercept = true,
+require("yankdown").setup()
+
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function(ev)
+    vim.keymap.set({ "n", "x", "i" }, "gp", "<Plug>(yankdown-paste-after)", { buffer = ev.buf })
+  end,
 })
