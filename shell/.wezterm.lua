81d92c730a29c58c9a02b89f547372a4980b4971
edunga1 <goonr21@gmail.com>
2026-09-09 09:01:21 +0900

    Remove wezterm Left/RightArrow keybindings
    conflicted with vim keybindings

--- shell/.wezterm.lua
+++ shell/.wezterm.lua
@@ -5,11 +5,6 @@ config.font = wezterm.font 'JetBrains Mono'
 
 config.window_close_confirmation = 'NeverPrompt'
 
-config.keys = {
-  { key = 'LeftArrow', mods = 'OPT', action = wezterm.action.SendKey { key = 'b', mods = 'ALT' } },
-  { key = 'RightArrow', mods = 'OPT', action = wezterm.action.SendKey { key = 'f', mods = 'ALT' } },
-}
-
 config.window_background_opacity = 0.7
 
 return config
