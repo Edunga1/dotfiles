cc0fb21da19105500f1f715e31c33862c4c5d631
edunga1 <goonr21@gmail.com>
2026-09-21 17:45:41 +0900

    Remove a vim imap gp binding

--- vim/vim-include/yankdown.lua
+++ vim/vim-include/yankdown.lua
@@ -8,6 +8,6 @@ require("yankdown").setup()
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "markdown",
   callback = function(ev)
-    vim.keymap.set({ "n", "x", "i" }, "gp", "<Plug>(yankdown-paste-after)", { buffer = ev.buf })
+    vim.keymap.set({ "n", "x" }, "gp", "<Plug>(yankdown-paste-after)", { buffer = ev.buf })
   end,
 })
